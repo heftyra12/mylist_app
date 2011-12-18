8ac3f8d684f81359c64c8dc11acbf52e6db9d2fb
Rob Hefty <heftyra12@uww.edu>
2011-12-18 10:40:32 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MylistApp::Application.config.secret_token = 'f67aa466e0b8530d87216678758225bb8e36e50c427ecc9a09c53db9e44f7a4d25daee8968372779c424f218631f66ecd7e7f2b3e185bbe2fe34fdf4cbb5ffb3'
